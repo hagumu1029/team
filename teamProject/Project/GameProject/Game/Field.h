c60d590184598a4cc38a82b732ecedb71fdb8089
fuka <fuka.bigkatsu0531@icloud.com>
2024-09-19 14:07:55 +0900

    変更

--- teamProject/Project/GameProject/Game/Field.h
+++ teamProject/Project/GameProject/Game/Field.h
@@ -3,5 +3,23 @@
 
 class Field :public Base {
 private:
-	CImage 
+	//画像オブジェクト（前景）
+	CImage m_foreground;
+	//画像オブジェクト（遠景）
+	CImage m_background;
+	//画像オブジェクト（最遠景）
+	CImage m_sky;
+	//地面の高さ
+	float m_ground_y;
+
+public:
+	///コンストラクト
+	Field();
+	///描画
+	void Draw();
+
+	///地面の高さを取得
+	float GetGroundY() {
+		return m_ground_y;
+	}
 };
