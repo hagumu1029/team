3a780b7c5b2fa7328be3af100a0857729c1f5285
fuka <fuka.bigkatsu0531@icloud.com>
2024-10-04 11:55:04 +0900

    a

--- teamProject/Project/GameProject/main.cpp
+++ teamProject/Project/GameProject/main.cpp
@@ -132,7 +132,7 @@ void Init(void)
 	ADD_RESOURCE("MapTip", CImage::CreateImage("Image/pureiya(kari).png"));
 
 	Base::Add(new Field());
-	//Base::Add(new Title());
+	Base::Add(new Title());
 	Base::Add(new Map());
 
 
