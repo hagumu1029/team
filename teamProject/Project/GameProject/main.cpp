3c8a0503b26b91779f0bcb17f5256825afa7ad58
clover0852 <leo10251025@gmail.com>
2024-10-10 11:40:33 +0900

    a

--- teamProject/Project/GameProject/main.cpp
+++ teamProject/Project/GameProject/main.cpp
@@ -132,7 +132,7 @@ void Init(void)
 
 
 
-	ADD_RESOURCE("MapTip", CImage::CreateImage("Image/Map.png"));
+	ADD_RESOURCE("MapTip", CImage::CreateImage("Image/1.Map(2).png"));
 
 	Base::Add(new Field());
 	Base::Add(new Title());
